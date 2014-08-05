d5c4731ff4f890bbf94d341e2bb36076debf2672
Steven Silvester <steven.silvester@ieee.org>
2014-08-05 16:29:31 -0500

    Move to test_scilabmagic

--- scilab2py/ipython/tests/test_scilabmagic.py
+++ scilab2py/ipython/tests/test_scilabmagic.py
@@ -1,4 +1,4 @@
-"""Tests for Octave magics extension."""
+"""Tests for Scilab magics extension."""
 import codecs
 import unittest
 import sys
@@ -6,12 +6,12 @@ from IPython.testing.globalipapp import get_ipython
 
 try:
     import numpy.testing as npt
-    from oct2py.ipython import octavemagic
+    from scilab.ipython import scilabmagic
 except Exception:  # pragma: no cover
     __test__ = False
 
 
-class OctaveMagicTest(unittest.TestCase):
+class ScilabMagicTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -25,35 +25,35 @@ class OctaveMagicTest(unittest.TestCase):
         cls.ip = get_ipython()
         # This is just to get a minimally modified version of the changes
         # working
-        cls.ip.magic('load_ext oct2py.ipython')
+        cls.ip.magic('load_ext scilab2py.ipython')
         cls.ip.ex('import numpy as np')
         cls.svgs_generated = 0
 
-    def test_octave_inline(self):
-        result = self.ip.run_line_magic('octave', '[1, 2, 3] + 1;')
+    def test_scilab_inline(self):
+        result = self.ip.run_line_magic('scilab', '[1, 2, 3] + 1;')
         npt.assert_array_equal(result, [[2, 3, 4]])
 
-    def test_octave_roundtrip(self):
+    def test_scilab_roundtrip(self):
         ip = self.ip
         ip.ex('x = np.arange(3); y = 4.5')
-        ip.run_line_magic('octave_push', 'x y')
-        ip.run_line_magic('octave', 'x = x + 1; y = y + 1;')
-        ip.run_line_magic('octave_pull', 'x y')
+        ip.run_line_magic('scilab_push', 'x y')
+        ip.run_line_magic('scilab', 'x = x + 1; y = y + 1;')
+        ip.run_line_magic('scilab_pull', 'x y')
 
         npt.assert_array_equal(ip.user_ns['x'], [[1, 2, 3]])
         npt.assert_equal(ip.user_ns['y'], 5.5)
 
-    def test_octave_cell_magic(self):
+    def test_scilab_cell_magic(self):
         ip = self.ip
         ip.ex('x = 3; y = [1, 2]')
-        ip.run_cell_magic('octave', '-f png -s 400,400 -i x,y -o z',
+        ip.run_cell_magic('scilab', '-f png -s 400,400 -i x,y -o z',
                           'z = x + y;')
         npt.assert_array_equal(ip.user_ns['z'], [[4, 5]])
 
-    def test_octave_plot(self):
-        magic = self.ip.find_cell_magic('octave').__self__
+    def test_scilab_plot(self):
+        magic = self.ip.find_cell_magic('scilab').__self__
         magic._publish_display_data = self.verify_publish_data
-        self.ip.run_cell_magic('octave', '-f svg -s 400,500',
+        self.ip.run_cell_magic('scilab', '-f svg -s 400,500',
             'plot([1, 2, 3]); figure; plot([4, 5, 6]);')
         npt.assert_equal(self.svgs_generated, 2)
 
@@ -65,32 +65,32 @@ class OctaveMagicTest(unittest.TestCase):
 
             self.svgs_generated += 1
 
-    def test_octavemagic_localscope(self):
+    def test_scilabmagic_localscope(self):
         ip = self.ip
         ip.push({'x': 0})
-        ip.run_line_magic('octave', '-i x -o result result = x+1')
+        ip.run_line_magic('scilab', '-i x -o result result = x+1')
         result = ip.user_ns['result']
         npt.assert_equal(result, 1)
 
-        ip.run_cell('''def octavemagic_addone(u):
-        %octave -i u -o result result = u+1
+        ip.run_cell('''def scilabmagic_addone(u):
+        %scilab -i u -o result result = u+1
         return result''')
-        ip.run_cell('result = octavemagic_addone(1)')
+        ip.run_cell('result = scilabmagic_addone(1)')
         result = ip.user_ns['result']
         npt.assert_equal(result, 2)
 
         npt.assert_raises(
             KeyError,
             ip.run_line_magic,
-            "octave",
+            "scilab",
             "-i var_not_defined 1+1")
 
-    def test_octave_syntax_error(self):
+    def test_scilab_syntax_error(self):
         try:
-            self.ip.run_cell_magic('octave', '', "a='1")
-        except octavemagic.OctaveMagicError:
-            self.ip.magic('reload_ext oct2py.ipython')
+            self.ip.run_cell_magic('scilab', '', "a='1")
+        except scilabmagic.ScilabMagicError:
+            self.ip.magic('reload_ext scilab2py.ipython')
 
-    def test_octave_error(self):
-        npt.assert_raises(octavemagic.OctaveMagicError, self.ip.run_cell_magic,
-                          'octave', '', 'a = ones2(1)')
+    def test_scilab_error(self):
+        npt.assert_raises(scilabmagic.ScilabMagicError, self.ip.run_cell_magic,
+                          'scilab', '', 'a = ones2(1)')
